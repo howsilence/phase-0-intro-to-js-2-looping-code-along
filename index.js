83c373efc344dfe850463ef024c146e2c9707a4f
howsilence <statton0104@gmail.com>
2022-01-12 09:15:16 -0500

    commit tree ea97c9c1c7a5e4de692e5c2af2a98347c8170cba

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(name, day){
+    let finishedCards = []
+    for (let i = 0; i < name.length; i++){
+        finishedCards.push(`Thank you, ${name[i]}, for the wonderful ${day} gift!`)
+    }
+    return finishedCards
+};
+function countDown(num){
+    let i = num
+    while (i >= 0){
+        console.log(i)
+        i--
+    }
+};
